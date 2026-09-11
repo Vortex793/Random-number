c48d3c01f028a34b7b0f476560794c3091fd9f42
339821175 <339821175@2260I-L29-020>
2026-09-11 13:08:12 -0400

    Add project files.

---
+++ Random num/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Which part of the code would you like to run (choose between 1-4)");
+            Console.WriteLine("1 - Random Numbers");
+            Console.WriteLine("2 - Adder");
+            Console.WriteLine("3 - Distance");
+            Console.WriteLine("4 - Hypotenuse");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1) Part1();
+            else if (choice == 2) Part2();
+            else if (choice == 3) Part3();
+            //else if (choice == 4) Part4();
+
+            else Console.WriteLine("Invalid number :(");
+
+            Console.ReadLine();
+
+        
+            
+        }
+
+        static void Part1()
+        {
+            Random generator = new Random();
+
+            int minNum, maxNum;
+
+            Console.Write("Enter the minimum number: ");
+            minNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the maximum number: ");
+            maxNum = Convert.ToInt32(Console.ReadLine());
+
+            if (minNum > maxNum)
+            {
+                Console.WriteLine("Minimum number cannot be greater than maximum number.");
+                return;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                int randomNum = generator.Next(minNum, maxNum + 1);
+                Console.WriteLine($"Random number {i + 1}: {randomNum}");
+            }
+        }
+
+        static void Part2()
+        {
+            Console.Clear();
+            Random generator = new Random();
+
+            int dice1 = generator.Next(1, 7);
+            int dice2 = generator.Next(1, 7);
+            int sum = dice1 + dice2;
+
+            Console.Write("Hit enter to roll the 2 dice: ");
+            Console.ReadLine();
+
+            Console.WriteLine($"Dice 1: {dice1}");
+            Console.WriteLine($"Dice 2: {dice2}");
+            Console.WriteLine($"Sum: {sum}");
+        }
+
+        static void Part3()
+        {
+            Console.Clear();
+
+            int minNum, maxNum;
+
+            Console.Write("Enter the minimum number: ");
+            minNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the maximum number: ");
+            maxNum = Convert.ToInt32(Console.ReadLine());
+
+            if (minNum > maxNum)
+            {
+                Console.WriteLine("Minimum number cannot be greater than maximum number.");
+                return;
+            }
+
+        }
+    }
+}
---
+++ Random num/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Random num")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TVDSB")]
+[assembly: AssemblyProduct("Random num")]
+[assembly: AssemblyCopyright("Copyright © TVDSB 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("229864ed-b166-4e82-bad3-d6d0d7955595")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
